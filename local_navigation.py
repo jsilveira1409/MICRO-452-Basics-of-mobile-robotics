c0863d15d1e3c5c0d94a5bbb80833c2096c4b2bc
Kyan Achtari <kyan.achtari@epfl.ch>
2022-11-29 17:09:29 +0100

    test

--- local_navigation.py
+++ local_navigation.py
@@ -1,11 +1,8 @@
+import numpy as np
+from tdmclient import ClientAsync
 
-i = 0
-while (True) {
-
-i ++;
-
-if(i == 15){
-    break
-}   
-
-}
+def avoid_obstacle(x,y):
+    dist_l = 2*prox.horizontal[0] + prox.horizontal[1]
+    dist_r = 2*prox.horizontal[4] + prox.horizontal[3]
+    motor.left.target = motor.left.target + dist_l // 10
+    motor.right.target = motor.right.target + dist_r // 10
