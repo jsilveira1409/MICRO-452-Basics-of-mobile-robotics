f92aa03e915a2e6168c0b908fff36f1b7553607e
Kyan Achtari <kyan.achtari@epfl.ch>
2022-12-01 14:43:13 +0100

    eviter

--- local_navigation.py
+++ local_navigation.py
@@ -1,8 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from tdmclient import ClientAsync
 
-def avoid_obstacle(x,y):
-    dist_l = 2*prox.horizontal[0] + prox.horizontal[1]
-    dist_r = 2*prox.horizontal[4] + prox.horizontal[3]
-    motor.left.target = motor.left.target + dist_l // 10
-    motor.right.target = motor.right.target + dist_r // 10
+motor_left_target = 100
+motor_right_target = 100
+
+# distance = -339.47*(sensor_value) + 5739.9
+
+@onevent
+def prox():
+    global prox_horizontal, motor_left_target, motor_right_target
+    print(1)
+    # acquisition from the proximity sensors to detect obstacles
+    motor_left_target = 100 + 2 * (prox_horizontal[0] // 100)
+    motor_right_target = 100 + 2 * (prox_horizontal[4] // 100)
+    leds_top = [30,30,30]
